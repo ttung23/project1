ed3ad502fd6e37feaa1b8b7bc45164a1fbe8a777
Nguyên Thành Luân <Luanntph25463>
2022-11-13 00:14:52 +0700

    add source

--- admin/room/danh-sach.php
+++ admin/room/danh-sach.php
@@ -1,12 +1,158 @@
-<div class="row container m-auto ">
-    <aside class="col-sm-3 bg-primary h-auto">
-        <div>
-            sss
+<div class="border border-[#D0D7DE] fs-6">
+    <div class="pb-4 m-4">
+        <h1 class="fs-6 text-primary"><i class="fa-solid fa-hotel mr-4"></i>Sơ Đồ Phòng</h1>
+        <div class="col-sm-2">
+            <label for="">Loại Phòng:</label>
+            <select name="" id="" class="form-control">
+                <option value="">Tất Cả</option>
+                <option value="">Tất Cả</option>
+                <option value="">Tất Cả</option>
+                <option value="">Tất Cả</option>
+                <option value="">Tất Cả</option>
+            </select>
         </div>
-    </aside>
-    <article class="col-sm-8 bg-primary h-auto ">
-        <div>
-ssss
+        <div class="my-4 row">
+            <label for="">Trạng Thái Phòng</label>
+            <div class="col-sm-2 m-4 hover-bg-[#78C2B5]">
+                <div>
+                    <button class="border btn btn-outline-success border-[#D0D7DE] w-100 h-100 text-primary px-3 py-1 shadow-lg">
+                        Tất Cả
+                    </button>
+                </div>
+            </div>
+            <div class="col-sm-2 m-4">
+                <div>
+                    <button class="border btn btn-outline-danger border-[#D0D7DE] w-100 h-100 text-primary px-3 py-1 shadow-lg">
+                    <i class="fa-solid fa-person-walking-luggage text-primary"></i>
+                        Phòng Đang Đến
+                    </button>
+                </div>
+            </div>
+            <div class="col-sm-2 m-4">
+                <div>
+                    <button class="border border-[#D0D7DE] btn btn-outline-warning btn btn-outline-danger w-100 h-100 text-primary px-3 py-1 shadow-lg">
+                    <i class="fa-solid fa-person-walking-luggage text-success"></i>
+
+                        phòng Đang Sửa
+                    </button>
+                </div>
+            </div>
+            <div class="col-sm-2 m-4">
+                <div>
+                    <button class="border border-[#D0D7DE] btn btn-outline-warning btn btn-outline-danger w-100 h-100 text-primary px-3 py-1 shadow-lg">
+                    <i class="fa-solid fa-person-walking-luggage text-success"></i>
+
+
+                        phòng Đang Sửa
+                    </button>
+                </div>
+            </div>
+            <div class="col-sm-2 m-4">
+                <div>
+                    <button class="border border-[#D0D7DE] btn btn-outline-warning btn btn-outline-danger w-100 h-100 text-primary px-3 py-1 shadow-lg">
+                    <i class="fa-solid fa-person-walking-luggage text-success"></i>
+
+
+                        phòng Đang Sửa
+                    </button>
+                </div>
+            </div>
+            <div class="col-sm-2 m-4">
+                <div>
+                    <button class="border border-[#D0D7DE] btn btn-outline-warning btn btn-outline-danger w-100 h-100 text-primary px-3 py-1 shadow-lg">
+                    <i class="fa-solid fa-person-walking-luggage text-success"></i>
+
+
+                        phòng Đang Sửa
+                    </button>
+                </div>
+            </div>
+            <div class="col-sm-2 m-4">
+                <div>
+                    <button class="border border-[#D0D7DE] btn btn-outline-warning btn btn-outline-danger w-100 h-100 text-primary px-3 py-1 shadow-lg">
+                    <i class="fa-solid fa-person-walking-luggage text-success"></i>
+
+                        phòng Đang Sửa
+                    </button>
+                </div>
+            </div>
+            <div class="col-sm-2 m-4">
+                <div>
+                    <button class="border border-[#D0D7DE] btn btn-outline-warning btn btn-outline-danger w-100 h-100 text-primary px-3 py-1 shadow-lg">
+                    <i class="fa-solid fa-person-walking-luggage text-success"></i>
+
+                        phòng Đang Sửa
+                    </button>
+                </div>
+            </div>
         </div>
-    </article>
+        <div>
+            *<a class="text-primary px-4">di chuyển con trỏ chọn hình ảnh để xem thông tin phòng</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-1 m-5 px-2 text-center py-5 bg-success border border-[#D0D7DE] shadow-lg rounded">
+                    <div class="  text-white">
+                    <h1 class="fs-4">P301</h1>
+                    <h2 class="fs-5">Víp</h2>
+                    <div class="bg-light rounded-circle w-50 text-center mx-3">
+                        <h3 class="fs-5"><i class="fa-solid fa-check text-success"></i></h3>
+                    </div>
+                </div>
+        </div>
+        <div class="col-sm-1 m-5 px-2 text-center py-5 bg-success border border-[#D0D7DE] shadow-lg rounded">
+                    <div class="  text-white">
+                    <h1 class="fs-4">P301</h1>
+                    <h2 class="fs-5">Víp</h2>
+                    <div class="bg-light rounded-circle w-50 text-center mx-3">
+                        <h3 class="fs-5"><i class="fa-solid fa-check text-success"></i></h3>
+                    </div>
+                </div>
+        </div>
+        <div class="col-sm-1 m-5 px-2 text-center py-5 bg-success border border-[#D0D7DE] shadow-lg rounded">
+                    <div class="  text-white">
+                    <h1 class="fs-4">P301</h1>
+                    <h2 class="fs-5">Víp</h2>
+                    <div class="bg-light rounded-circle w-50 text-center mx-3">
+                        <h3 class="fs-5"><i class="fa-solid fa-check text-success"></i></h3>
+                    </div>
+                </div>
+        </div>
+        <div class="col-sm-1 m-5 px-2 text-center py-5 bg-success border border-[#D0D7DE] shadow-lg rounded">
+                    <div class="  text-white">
+                    <h1 class="fs-4">P301</h1>
+                    <h2 class="fs-5">Víp</h2>
+                    <div class="bg-light rounded-circle w-50 text-center mx-3">
+                        <h3 class="fs-5"><i class="fa-solid fa-check text-success"></i></h3>
+                    </div>
+                </div>
+        </div>
+        <div class="col-sm-1 m-5 px-2 text-center py-5 bg-success border border-[#D0D7DE] shadow-lg rounded">
+                    <div class="  text-white">
+                    <h1 class="fs-4">P301</h1>
+                    <h2 class="fs-5">Víp</h2>
+                    <div class="bg-light rounded-circle w-50 text-center mx-3">
+                        <h3 class="fs-5"><i class="fa-solid fa-check text-success"></i></h3>
+                    </div>
+                </div>
+        </div>
+        <div class="col-sm-1 m-5 px-2 text-center py-5 bg-success border border-[#D0D7DE] shadow-lg rounded">
+                    <div class="  text-white">
+                    <h1 class="fs-4">P301</h1>
+                    <h2 class="fs-5">Víp</h2>
+                    <div class="bg-light rounded-circle w-50 text-center mx-3">
+                        <h3 class="fs-5"><i class="fa-solid fa-check text-success"></i></h3>
+                    </div>
+                </div>
+        </div>
+        <div class="col-sm-1 m-5 px-2 text-center py-5 bg-success border border-[#D0D7DE] shadow-lg rounded">
+                    <div class="  text-white">
+                    <h1 class="fs-4">P301</h1>
+                    <h2 class="fs-5">Víp</h2>
+                    <div class="bg-light rounded-circle w-50 text-center mx-3">
+                        <h3 class="fs-5"><i class="fa-solid fa-check text-success"></i></h3>
+                    </div>
+                </div>
+        </div>
+    </div>
 </div>
