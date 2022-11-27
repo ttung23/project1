f1a2d3545fa77fc91e9be28798b7d82019d5e497
chuoi2taps <thiennnph25438@fpt.edu.vn>
2022-11-27 15:54:15 +0700

    update cart

--- layout/assets/js/main.js
+++ layout/assets/js/main.js
@@ -1,8 +1,7 @@
 function hamDropdown() {
-  var element = document.getElementById("money");
+  var element = document.getElementById("logo");
   element.classList.toggle("hien");
 }
-
 function drop_menu() {
   var element = document.getElementById("login");
   element.classList.toggle("hien-login");
