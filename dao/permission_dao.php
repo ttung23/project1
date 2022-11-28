109b71406cf46d0dd8f38296cfa7535c87938a7f
luanntph25463 <luanntph25463@fpt.edu.vn>
2022-11-28 20:25:46 +0700

    merge thien

--- dao/permission_dao.php
+++ dao/permission_dao.php
@@ -1,11 +1,6 @@
 <?php
 function loadAll_permissions(){
-<<<<<<< HEAD
-    $query = "select * from permissions order by permissions desc";
-    $listservice = pdo_query_all($query);
-    return $listservice;
-}
-=======
+
     $query = "select * from permissions order by permission_id desc";
     $listservice = pdo_query_all($query);
     return $listservice;
@@ -15,8 +10,6 @@ function loadAll_permissionsone($id){
     $room = pdo_query_one($query,$id);
     return $room;
 }
->>>>>>> c8d86ca (add file)
-
 function permissions_remove_by_id($cate_id){
     $query = "delete from permissions where permission_id = ?";
     pdo_execute($query, $cate_id);
@@ -25,17 +18,10 @@ function Insert_permissions($name,$description){
     $query = "Insert into permissions(name,description) values(?,?)";
     pdo_execute($query, $name,$description);
 }
-<<<<<<< HEAD
-function Update_permissions($name,$description)
-{
-    $query = "Update permissions set name = ? ,description = ? where permission_id = ?";
-    pdo_execute($query,$name,$description);
-=======
 function Update_permissions($name,$description,$id)
 {
     $query = "Update permissions set name = ? ,description = ? where permission_id = ?";
     pdo_execute($query,$name,$description,$id);
->>>>>>> c8d86ca (add file)
 }
 
 ?>
