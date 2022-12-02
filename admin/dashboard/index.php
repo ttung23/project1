1f6ab32ddf78fc2fc741b37d19b69bbddc160478
luanntph25463 <luanntph25463@fpt.edu.vn>
2022-12-03 01:28:51 +0700

    add file

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -12,15 +12,15 @@
     require_once '../../dao/user_dao.php';
 
     // CONTROLLER
-    if (isset($_SESSION['admin'])) {
+    // if (isset($_SESSION['admin'])) {
         $VIEW_TITLE = "Bảng điều khiển";
         $VIEW_CSS = 'index-admin.css';
         $VIEW_ADMIN_NAME = '../dashboard/admin_index.php';
 
         include_once '../templates/layout/layout.php';
-    } else {
-        header("location: ../index.php");
-    }
+    // } else {
+    //     header("location: ../index.php");
+    // }
 
 
 ?>
